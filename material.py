10a6ec5d3cef56a9ff007263a6b7563dd1c64fac
Christos <giachoud@gapps.auth.gr>
2023-06-22 10:48:09 +0300

    Computing light

--- material.py
+++ material.py
@@ -1,4 +1,10 @@
 class PhongMaterial:
+    """
+    ka: coefficient of diffused light from the environment
+    kd: diffuse reflection coefficient
+    ks: specular reflection coefficient
+    n: Phong constant
+    """
     def __init__(self, ka, kd, ks, n):
         self.ka = ka
         self.kd = kd
--- pointLight.py
+++ pointLight.py
@@ -1,15 +1,40 @@
+import numpy as np
+
 def light(point, normal, vcolor, cam_pos, mat, lights):
-    I = 0
+    """
+    point: the 3d coordinates of the point on which the light arrives
+    normal: coordinates of the normal vector of the surface at the point
+    vcolor: the color of the point
+    cam_pos: coordinates of the camera
+    mat: PhongMaterial object
+    lights: list of PointLight objects
+    """
 
     # Ambient light component
     ambiance = []
     for i in range(len(lights)):
-        ambiance.append(vcolor + mat.ka * lights[0].intensity)
+        ambiance.append(mat.ka * lights[0].intensity)
 
     # Diffuse light component
+    diffusions = []
+    for i in range(len(lights)):
+        # Compute the L vector that is parallel to light beams
+        L = lights[i].pos - point
+        L = L / np.norm(L)
+        # Compute the fatt attenuation coefficient
+        fatt = 1/np.linalg.norm(L)
+        diffusions.append(lights[i].intensity * mat.kd * fatt * np.dot(normal, L))
 
     # Specular light component
+    speculars = []
+    V = (cam_pos - point) / np.norm(cam_pos - point)
+    dotNL = np.dot(normal, L)
+    for i in range(len(lights)):
+        L = (lights[i].pos - point) / np.norm((lights[i].pos - point))
+        fatt = 1/np.linalg.norm(L)
+        dotNL = np.dot(normal, L)
+        speculars.append(lights[i].intensity * fatt * mat.ks * (np.dot((2 * normal * dotNL - L), V) ^ mat.n))
 
     # Combination
-
+    I = vcolor + np.sum(ambiance) + np.sum(diffusions) + np.sum(speculars)
     return I
