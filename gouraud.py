09a7bb455fcb568757989688defdf9e1bb4af3d6
Christos <giachoud@gapps.auth.gr>
2023-06-23 13:02:23 +0300

    gouraud initiative

--- gouraud.py
+++ gouraud.py
@@ -1,3 +1,16 @@
-def shade_gouraud(vertsp, vertsn, vertsc, bcoords, cam_pos, mat, lights, light_amb,X):
-    img = []
-    return img
+def shade_gouraud(vertsp, vertsn, vertsc, bcoords, cam_pos, mat, lights, light_amb, X):
+    """
+    vertsp = 2x3 array, the 2d coordinates for each vertex
+    vertsn = 3x3 array, the normal vectors of the triangle
+    vertsc = 3x3 array, the colors of the vertices
+    bcoords = 3x1 array, the gravity center of the triangle
+    cam_pos = 3x1 array, the coordinates of the camera center
+    mat = a PhongMaterial object
+    lights = a list of PointLight objects
+    light_amb = the ambiance
+    X = the partially painted canvas
+    """
+
+    
+
+    return X
--- photography.py
+++ photography.py
@@ -7,20 +7,20 @@ import statistics as stats
 
 def render_object(shader, focal, eye, lookat, up, bg_color, M, N, H, W, verts, vert_colors, faces, mat, lights, light_amb):
     """
-    % shader = the string that defines what shading method is selected
-    % focal = the distance of the frame from the camera center
-    % eye = the coordinates of the camera
-    % lookat = the coordinates of where the camera looks
-    % up = the up vector of the camera
-    % bg_color = the background color of the canvas
-    % M, N = the dimensions of the final image
-    % H, W = the dimensions of the frame of the camera
-    % verts = 3xNv array, the vertices of the triangles
-    % vert_colors = 3xNv array, the color for each vertex
-    % faces = 3xNt array, the points that shape the triangles
-    % mat = a PhongMaterial object
-    % lights = a list of PointLight objects
-    % light_amb = the ambiance
+    shader = the string that defines what shading method is selected
+    focal = the distance of the frame from the camera center
+    eye = the coordinates of the camera
+    lookat = the coordinates of where the camera looks
+    up = the up vector of the camera
+    bg_color = the background color of the canvas
+    M, N = the dimensions of the final image
+    H, W = the dimensions of the frame of the camera
+    verts = 3xNv array, the vertices of the triangles
+    vert_colors = 3xNv array, the color for each vertex
+    faces = 3xNt array, the points that shape the triangles
+    mat = a PhongMaterial object
+    lights = a list of PointLight objects
+    light_amb = the ambiance
     """
     # Calculate normal vectors
     normals = calculate_normals(verts, faces)
