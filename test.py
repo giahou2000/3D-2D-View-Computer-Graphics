1b55979c0e5de2c95b050f68186f15157e636987
Christos <giachoud@gapps.auth.gr>
2023-06-24 19:42:05 +0300

    Testing

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import numpy as np
+import matplotlib.pyplot
 
-diffusions = []
-diffusions.append([5,6,7])
-diffusions.append([5,6,7])
-diffusions.append([5,6,7])
-print(np.sum(diffusions, axis=0))
+data = np.load('h3.npy', allow_pickle=True)
+M, N, W, H = data[()]['M'], data[()]['N'], data[()]['W'], data[()]['H']
+bg_color = data[()]['bg_color']
+
+print(bg_color)
+
+img = np.full((M, N, 3), [0.42, 0.734, 0.245])
+
+matplotlib.pyplot.imshow(img)
+matplotlib.pyplot.show()
